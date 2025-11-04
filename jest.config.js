4999fb03c9fa14710b5d52f2dd2a38240f50bccd
Matheus Costa de Lima <matheuscosta77a@gmail.com>
2025-11-03 23:16:19 -0300

    Adicionando o JEST e realizando testes automatizados

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  verbose: true,
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
--- server/server.js
+++ server/server.js
@@ -192,3 +192,5 @@ app.get('/dashboard', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+module.exports = app;
